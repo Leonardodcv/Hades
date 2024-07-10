bbf2546b5d468ba2bc6e8c83fefac18b8f8936ab
Leonardodcv <leodancarvelmail.com>
2024-07-10 11:04:55 -0600

    modelos de usuario

--- Hades/settings.py
+++ Hades/settings.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
-
+#import * from "./db"
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #Instaladas
+
+    #creadas
+    "erp",
+
 ]
 
 MIDDLEWARE = [
@@ -72,11 +77,24 @@ WSGI_APPLICATION = 'Hades.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+"""
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATABASES = {
+    "default" : {
+        "ENGINE" : "django.db.backends.sqlite3",
+        "NAME" : os.path.join(BaseException, "db/sqlite/db.sqlite3"),
+    }
+}"""
+
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default" : {
+        "ENGINE" : "django.db.backends.postgresql_psycopg2",
+        "NAME" : "db",
+        "USER" : "postgres",
+        "PASSWORD" : "qwe123",
+        "HOST" : "localhost",
+        "PORT" : "5432"
     }
 }
 
