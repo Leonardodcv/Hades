03818e7163d00947e6f3ceec09088aad87d079c2
Leonardodcv <leodancarvelmail.com>
2024-07-30 18:08:28 -0600

    Vista basada en funciones

--- erp/urls.py
+++ erp/urls.py
@@ -1,5 +1,8 @@
-app_name="erp"
+from django.urls import path
+from erp.views.category.views import category_list
 
-urlpatterns =[
-    path("category/list/", category_list, name="category_list"),
+app_name = 'erp'
+
+urlpatterns = [
+    path('category/list/', category_list, name='category_list'),
 ]
--- erp/views/category/views.py
+++ erp/views/category/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 
+from erp.models import Category
+
+
 def category_list(request):
-    data ={
-
+    data = {
+        'title': 'Listado de Categorías',
+        'categories': Category.objects.all()
     }
-
-    return render(request, "category/list.html", data)
+    return render(request, 'category/list.html', data)
